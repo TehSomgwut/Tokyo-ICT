ae11e9fe47bf5af99f52f00a93d99faab01790cc
Teh Songwut <phosrisongwut@gmail.com>
2025-09-23 14:18:35 +0700

    Add database

---
+++ database/Customer.js
@@ -0,0 +1,30 @@
+const mongoose = require('../connectDB');
+
+const customerSchema = new mongoose.Schema({
+    Cusname: String,
+    CusGmail: {type: String, unique: true, request: true},
+    Password: String,
+    Img_profile: Buffer,
+    Status: String,
+    Current_order: [
+        {
+            Order_name: String,
+            Order_price: Number,
+            Money_state: String,
+            quantity: Number,
+            State: String,
+            Total_price: Number,
+        }
+    ],
+    History_order: [
+        {
+            Order_date: Date,
+            order_name: Number,
+            quantity: Number,
+        }
+    ]
+})
+
+const Customer = mongoose.model('Customer', customerShema);
+
+module.exports = Customer;

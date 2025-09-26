bab9e60d291d08cb12ab46fec8e3276555f0e2ab
Teh Songwut <phosrisongwut@gmail.com>
2025-09-26 16:27:26 +0700

    Template main

--- routes/main_routing.js
+++ routes/main_routing.js
@@ -5,7 +5,63 @@ const path = require('path')
 require('../connectDB')
 
 router.all('/main', (req, res) => {
-    res.render('Main');
+    const recommendedMenus = [
+        {
+        name: "tokyo",
+        description: "Delicious tokyo",
+        price: 10
+        },
+        {
+        name: "sweet tokyo",
+        description: "Top delicious tokyo",
+        price: 10
+        },
+        {
+        name: "l-tokyo",
+        description: "Good delicious tokyo",
+        price: 10
+        }
+    ]
+
+    const categories = [
+        {
+            name: "Top sales",
+            image: "#"
+        }
+    ]
+
+    const menus = [
+        {
+            name: "Tokyo",
+            flavour: "เค็ม",
+            price: 10,
+            imgPath: "#"
+        },
+        {
+            name: "Tokyo",
+            flavour: "หวาน",
+            price: 10,
+            imgPath: "#"
+        },
+        {
+            name: "Tokyo",
+            flavour: "สลัด",
+            price: 10,
+            imgPath: "#"
+        },
+        {
+            name: "Tokyo",
+            flavour: "ไม่มีใส้",
+            price: 15,
+            imgPath: "#"
+        },
+    ]
+    res.render('Main', {
+        recommendedMenus: recommendedMenus,
+        categories: categories,
+        name: req.session.email,
+        menus: menus
+    });
 })
 
 module.exports = router;

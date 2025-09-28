b6c28b95076a92b90a38f5be1f64fa72992d3c97
Teh Songwut <phosrisongwut@gmail.com>
2025-09-28 19:04:33 +0700

    Update seller

--- routes/seller_edit_route.js
+++ routes/seller_edit_route.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const Tokyo = require('../database/Tokyo');
+const Crapes = require('../database/Crapes');
+const Fried = require('../database/Fried');
+const Topping = require('../database/Flavor');
 
 router.get('/seller_edit', (req, res) => {
     const selleredit = {
-        Store_status: "เปิด"
+        Name: "ชื่อเมนู",
+        Detail: "รายละเอียดเมนู",
+        Price: 0,
+        Category: "โตเกียว",
+        Img_menu: ""
     }
     res.render('seller_edit', {seller_edit: selleredit})
+})
+
+router.get('/seller_edit/product/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        let menu = await Tokyo.findById(id) || await Crapes.findById(id) || await Fried.findById(id) || await Topping.findById(id);
+        if (!menu) {
+            return res.status(404).json({ message: 'Menu not found' });
+        }
+        console.log(menu);
+        res.render('seller_edit', {seller_edit: menu})
+    } catch (error) {
+        console.error('Error updating menu:', error);
+        res.status(500).json({ message: 'Error updating menu' });
+    }
 })
 
 module.exports = router
--- routes/seller_manipulation.js
+++ routes/seller_manipulation.js
@@ -4,24 +4,47 @@ const path = require('path');
 const Tokyo = require('../database/Tokyo');
 const Crapes = require('../database/Crapes');
 const Fried = require('../database/Fried');
+const Topping = require('../database/Flavor');
+const fs = require('fs');
 
 router.get('/seller_manipulation', async (req, res) => {
-    const sellermanipulation = [{
-        Store_status: "เปิด",
-        Menusname: "โตเกียว (ใส้เค็ม)จ้าา",
-        Detail: "โตเกียวเบสิค ไส้เค็มไม่กินถือว่าพลาดจ้าา",
-        Price: 30,
-        Img_menus: "",
-        Tagmenu: "โตๆ"
-    }]
-
     const tokyoMenus = await Tokyo.find({});
     const crapesMenus = await Crapes.find({});
     const friedMenus = await Fried.find({});
+    const toppingMenus = await Topping.find({});
+    const sellermanipulation = [
+    ...tokyoMenus,
+    ...crapesMenus,
+    ...friedMenus,
+    ...toppingMenus
+]
+
 
     console.log(tokyoMenus);
 
     res.render('seller_manipulation', {seller_manipulation: sellermanipulation})
+})
+
+router.post('/seller_delete', async (req, res) => {
+    const { id } = req.body;
+    try {
+        menu = await Tokyo.findById(id) || await Crapes.findById(id) || await Fried.findById(id) || await Topping.findById(id);
+        if (!menu) {
+            return res.status(404).json({ message: 'Menu not found' });
+        }
+        ImgPath = path.join(__dirname, `../public/${menu.Img}`);
+        if (fs.existsSync(menu.Img) && menu.Img) {
+            fs.unlinkSync(ImgPath);
+        }
+
+        await menu.deleteOne();
+        
+        const success = true;
+        res.json({ success, message: 'Menu deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting menu:', error);
+        res.status(500).json({ message: 'Error deleting menu' });
+    }
 })
 
 module.exports = router
